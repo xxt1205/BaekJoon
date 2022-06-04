a849c44c38a0f2463b6aa0b68c212e4641a95e37
JackGhL <xxt1205@gmail.com>
2022-06-05 00:43:35 +0900

    더하기사이클
    하드코딩이 되어버렸다

--- src/더하기사이클.java
+++ src/더하기사이클.java
@@ -6,28 +6,57 @@ public class 더하기사이클 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		int A,B;
+		int A,B,I;
 		
-			A = sc.nextInt();
-			B = sc.nextInt();
+//			A = sc.nextInt();
+//			B = sc.nextInt();
+			I = 1;
+			System.out.println();
+			String str = String.valueOf(I);
+			A = 0;
+			B = 0;
+			if(I >=10) {
+				A = str.charAt(0) - '0';
+				B = str.charAt(1) - '0';
+			}else if(10 > I) {
+				A = 0;	
+				B = str.charAt(0) - '0';
+			}
 			
-			int end = A + B;
 			int result = 0;
 			int C = 0;
 			int count = 0;
-			while(end == result) {
+			int x = A;
+			int y = B;
+			
+			
+			do {
+				
+				System.out.println(x+","+y);
 				result = A + B;
-				B = A;
+				System.out.println("result : "+result);
+				
 				if(result < 10) {
-					A = result;
+					C = result;
 					count++;
+					
 				}else if(result >= 10) {
-					A = result % 10;
+					C = result % 10;
 					count++;
+					
 				}
-			}
+				A = B;
+				
+				B = C;
+				
+				System.out.println("count: " + count);
+				System.out.println("A : " + A);
+				System.out.println("B : "+ B);
+				System.out.println("x : " + x);
+				System.out.println("y : " + y);
+			}while(x != A || y != B) ;
 			System.out.println(count);
-		sc.close();
+		
 		
 	}
 }
