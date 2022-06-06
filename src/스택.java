9ab7cb339286054c6d97878e64798d61d83bf4ae
JackGhL <xxt1205@gmail.com>
2022-06-06 19:51:46 +0900

    내적

---
+++ src/스택.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class 스택 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		 
+		for(int i = 0; N > i; i++) {
+			
+		}
+	}
+
+}
